6e1e1aade9a591f0791425a9093bffb96714d7de
Chris Baldwin <chrisdbaldwin@gmail.com>
2016-05-01 20:07:26 -0500

    Added VR support and a playspace

---
+++ vr dev grounds/Assets/Scripts/Video.cs
@@ -0,0 +1,18 @@
+//Video.cs
+
+// Starts the default camera and assigns the texture to the current renderer
+using UnityEngine;
+using System.Collections;
+
+public class Webcam : MonoBehavior {
+    void Start() {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+    void Update() {
+
+    }
+}
