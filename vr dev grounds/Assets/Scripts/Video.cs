1fab504d983e509e43958723360a45809964e2ae
Chris Baldwin <chrisdbaldwin@gmail.com>
2016-05-09 01:55:17 -0500

    Built a small world

--- vr dev grounds/Assets/Scripts/Video.cs
+++ vr dev grounds/Assets/Scripts/Video.cs
@@ -2,17 +2,21 @@
 
 // Starts the default camera and assigns the texture to the current renderer
 using UnityEngine;
-using System.Collections;
+using System;
 
-public class Webcam : MonoBehavior {
-    void Start() {
-        WebCamTexture webcamTexture = new WebCamTexture();
-        Renderer renderer = GetComponent<Renderer>();
-        renderer.material.mainTexture = webcamTexture;
-        webcamTexture.Play();
-    }
+namespace VideoPlayback
+{
+	public class Webcam : MonoBehaviour 
+	{
+        void Start() {
+            WebCamTexture webcamTexture = new WebCamTexture();
+            Renderer renderer = GetComponent<Renderer>();
+            renderer.material.mainTexture = webcamTexture;
+            webcamTexture.Play();
+        }
 
-    void Update() {
+        void Update() {
 
+        }
     }
 }
