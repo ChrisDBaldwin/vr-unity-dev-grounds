00f97f91f548f3525b2ecacd1c1aa4c6f53a65cd
Chris Baldwin <chrisdbaldwin@gmail.com>
2016-05-09 01:56:58 -0500

    Revert "Revert "Built a small world""
    This reverts commit 0428d63d99db79a8189496eb342cae5fc747b85d.

---
+++ vr dev grounds/Assets/Scripts/ParticleHandler.cs
@@ -0,0 +1,24 @@
+//GameState.cs
+
+using UnityEngine;
+using System;
+
+
+public class ParticleHandler : MonoBehaviour 
+{
+	public GameObject particlePrefab;
+	void Start () {
+		particlePrefab = GetComponent<GameObject>();
+    }
+
+    void OnTriggerEnter (Collision col) {
+	    if (col.gameObject.tag == "Player") {
+	    	Instantiate(particlePrefab);
+	    }
+	    else{
+	    	Destroy(particlePrefab);
+	    }
+	}
+
+ 
+}
